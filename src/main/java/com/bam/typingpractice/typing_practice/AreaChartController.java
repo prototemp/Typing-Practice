a4c5c51e305111d986cc0f4c35497b73a5a5941a
yeetologist <71173561+yeetologist@users.noreply.github.com>
2023-06-06 18:49:42 +0700

    Add option chart per time mode

--- src/main/java/com/bam/typingpractice/typing_practice/AreaChartController.java
+++ src/main/java/com/bam/typingpractice/typing_practice/AreaChartController.java
@@ -5,6 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 
 import java.net.URL;
 import java.util.List;
@@ -22,14 +23,33 @@ public class AreaChartController implements Initializable {
     public AreaChart<String,Integer> histAreaChart;
     @FXML
     public Button btn_lineChart;
+    @FXML
+    public ChoiceBox<String> pilihWaktu;
+
+    public List<XYChart.Series<String, Integer>> seriesList = null;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        List<XYChart.Series<String, Integer>> seriesList = DBUtils.userHistoryChart(Player.userId);
+        pilihWaktu.getItems().addAll(PlayerHistory.waktu);
         btn_back.setOnAction(actionEvent -> DBUtils.changeScene(actionEvent,"sample.fxml","Main Menu",null,null));
         btn_table.setOnAction(actionEvent -> DBUtils.changeScene(actionEvent,"history.fxml","Main Menu",null,null));
         btn_barChart.setOnAction(actionEvent -> DBUtils.changeScene(actionEvent,"bar-chart.fxml","Main Menu",null,null));
         btn_lineChart.setOnAction(actionEvent -> DBUtils.changeScene(actionEvent,"line-chart.fxml","Bar Chart",null,null));
+        if(PlayerHistory.historyState == null || PlayerHistory.historyState.equals("All")){
+            seriesList = DBUtils.userHistoryChart(Player.userId,0);
+        }
+        else {
+            seriesList = DBUtils.userHistoryChart(Player.userId,DBUtils.getModeId(PlayerHistory.historyState));
+        }
         histAreaChart.getData().addAll(seriesList.get(0),seriesList.get(1),seriesList.get(2));
+        pilihWaktu.setOnAction(actionEvent -> {
+            histAreaChart.getData().clear();
+            int selectedIndex = pilihWaktu.getSelectionModel().getSelectedIndex() + 1;
+            PlayerHistory.historyState = pilihWaktu.getValue();
+            if (selectedIndex != 5) {
+                seriesList = DBUtils.userHistoryChart(Player.userId, selectedIndex);
+                histAreaChart.getData().addAll(seriesList.get(0),seriesList.get(1),seriesList.get(2));
+            }
+        });
     }
 }
--- src/main/java/com/bam/typingpractice/typing_practice/BarChartController.java
+++ src/main/java/com/bam/typingpractice/typing_practice/BarChartController.java
@@ -1,10 +1,13 @@
 package com.bam.typingpractice.typing_practice;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 
 import java.net.URL;
 import java.util.List;
@@ -22,14 +25,33 @@ public class BarChartController implements Initializable {
     public Button btn_lineChart;
     @FXML
     public Button btn_areaChart;
+    @FXML
+    public ChoiceBox<String> pilihWaktu;
+
+    public List<XYChart.Series<String, Integer>> seriesList = null;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        List<XYChart.Series<String, Integer>> seriesList = DBUtils.userHistoryChart(Player.userId);
+        pilihWaktu.getItems().addAll(PlayerHistory.waktu);
         btn_back.setOnAction(actionEvent -> DBUtils.changeScene(actionEvent,"sample.fxml","Main Menu",null,null));
         btn_table.setOnAction(actionEvent -> DBUtils.changeScene(actionEvent,"history.fxml","Main Menu",null,null));
         btn_lineChart.setOnAction(actionEvent -> DBUtils.changeScene(actionEvent,"line-chart.fxml","Main Menu",null,null));
         btn_areaChart.setOnAction(actionEvent -> DBUtils.changeScene(actionEvent,"area-chart.fxml","Main Menu",null,null));
+        if(PlayerHistory.historyState == null || PlayerHistory.historyState.equals("All")){
+            seriesList = DBUtils.userHistoryChart(Player.userId,0);
+        }
+        else {
+            seriesList = DBUtils.userHistoryChart(Player.userId,DBUtils.getModeId(PlayerHistory.historyState));
+        }
         histBarChart.getData().addAll(seriesList.get(0),seriesList.get(1),seriesList.get(2));
+        pilihWaktu.setOnAction(actionEvent -> {
+            histBarChart.getData().clear();
+            int selectedIndex = pilihWaktu.getSelectionModel().getSelectedIndex() + 1;
+            PlayerHistory.historyState = pilihWaktu.getValue();
+            if (selectedIndex != 5) {
+                seriesList = DBUtils.userHistoryChart(Player.userId, selectedIndex);
+                histBarChart.getData().addAll(seriesList.get(0),seriesList.get(1),seriesList.get(2));
+            }
+        });
     }
 }
